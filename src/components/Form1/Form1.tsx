c6bad092bf9c097346ec6535949bc4e8785b0a26
Gunsnfnr <a.j.p.010203@gmail.com>
2024-08-17 16:48:51 +0300

    feat: add email validation

--- src/components/Form1/Form1.tsx
+++ src/components/Form1/Form1.tsx
@@ -2,40 +2,46 @@ import { Link } from 'react-router-dom';
 import _ from './Form1.module.css';
 import { FormEvent, useRef, useState } from 'react';
 import { object, string, ValidationError, number } from 'yup';
+import LabelInput from '../LabelInput/LabelInput';
 
 const Form1 = () => {
   const [nameError, setNameError] = useState('');
   const [ageError, setAgeError] = useState('');
+  const [emailError, setEmailError] = useState('');
   const nameRef = useRef<HTMLInputElement>(null);
   const ageRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
 
   const handleSubmit = async (e: FormEvent): Promise<void | null> => {
     e.preventDefault();
-    if (!nameRef.current || !ageRef.current) return null;
+    if (!nameRef.current || !ageRef.current || !emailRef.current) return null;
     const userSchema = object({
       name: string()
         .required('Name is a required field. ')
         .matches(/^[A-Z]{1}[a-z]{1,}$/, ' Сapitalise first letter, use English alphabet'),
       age: number().typeError('Age must be a number.').positive(' Age must be a positive number.'),
+      email: string().required('Email is a required field. ').email('Email must be a valid email.'),
     });
     try {
       const user = await userSchema.validate(
-        { name: nameRef.current.value, age: ageRef.current.value },
+        { name: nameRef.current.value, age: ageRef.current.value, email: emailRef.current.value },
         { abortEarly: false },
       );
       console.log('user: ', user);
 
       setNameError('');
       setAgeError('');
+      setEmailError('');
     } catch (err: unknown) {
       if (err instanceof ValidationError) {
-        const errors: Record<string, string> = { name: '', age: '' };
+        const errors: Record<string, string> = { name: '', age: '', email: '' };
 
         err.inner.forEach((unit) => {
           if (unit.path) errors[unit.path] += unit.errors[0];
         });
         setNameError(errors.name);
         setAgeError(errors.age);
+        setEmailError(errors.email);
       }
     }
   };
@@ -53,29 +59,9 @@ const Form1 = () => {
           void handleSubmit(e);
         }}
       >
-        <div className={_.formUnit}>
-          <label htmlFor="name" className={_.name}>
-            Name
-            <input type="text" id="name" name="name" ref={nameRef} />
-          </label>
-          <span className={_.error}>{nameError}</span>
-        </div>
-
-        <div className={_.formUnit}>
-          <label htmlFor="age" className={_.age}>
-            Age
-            <input type="number" id="age" name="age" ref={ageRef} />
-          </label>
-          <span className={_.error}>{ageError}</span>
-        </div>
-
-        <div className={_.formUnit}>
-          <label htmlFor="email" className={_.email}>
-            Email
-            <input type="text" id="email" name="email" />
-          </label>
-          <span className={_.error}></span>
-        </div>
+        <LabelInput type="text" name="name" refName={nameRef} error={nameError} />
+        <LabelInput type="number" name="age" refName={ageRef} error={ageError} />
+        <LabelInput type="text" name="email" refName={emailRef} error={emailError} />
 
         <div className={_.formUnit}>
           <label htmlFor="password" className={_.password}>
---
+++ src/components/LabelInput/LabelInput.tsx
@@ -0,0 +1,27 @@
+import _ from '../Form1/Form1.module.css';
+
+interface Props {
+  type: 'text' | 'number' | 'email' | 'password';
+  name: string;
+  refName: React.RefObject<HTMLInputElement>;
+  error: string;
+}
+
+export default function LabelInput(props: Props) {
+  return (
+    <div className={_.formUnit}>
+      <label htmlFor={`${props.name}`} className={_[props.name]}>
+        {`${props.name}`}
+        <input type={`${props.type}`} id={`${props.name}`} name={`${props.name}`} ref={props.refName} />
+      </label>
+      <span className={_.error}>{props.error}</span>
+    </div>
+    //   <div className={_.formUnit}>
+    //   <label htmlFor="email" className={_.email}>
+    //     Email
+    //     <input type="text" id="email" name="email" />
+    //   </label>
+    //   <span className={_.error}></span>
+    // </div>
+  );
+}
